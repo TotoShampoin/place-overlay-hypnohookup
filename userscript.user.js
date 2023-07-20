7a9ce0be43eb6e86bda7c35a40fe7d227f12fa8a
Thomas Garnier <41761945+TotoShampoin@users.noreply.github.com>
2023-07-20 19:54:03 +0200

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/Place Overlay
 // @namespace    https://github.com/TotoShampoin/place-overlay-hypnohookup
-// @version      1.1.0
+// @version      1.1.1
 // @description  A visual overlay to show errors in tile colors of a desired image in r/place
 // @author       github.com/TotoShampoin
 // @match        https://garlic-bread.reddit.com/embed*
@@ -22,7 +22,7 @@ if (window.top !== window.self) {
           (function () {
               const img = document.createElement("img");
               img.src = "https://cdn.discordapp.com/attachments/669939027537035297/1131644724529344542/hooky.png";
-              img.style = "position: absolute;left:334px;top:144px; image-rendering: pixelated; opacity: .5";
+              img.style = "position: absolute;left:334px;top:144px; image-rendering: pixelated; opacity: 1";
               console.log(img);
               return img;
           })())
