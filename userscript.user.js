1f09f581f4896056f2600f5f203ce87edb47a022
Thomas Garnier <41761945+TotoShampoin@users.noreply.github.com>
2023-07-20 19:52:41 +0200

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/Place Overlay
 // @namespace    https://github.com/TotoShampoin/place-overlay-hypnohookup
-// @version      1.0.0
+// @version      1.1.0
 // @description  A visual overlay to show errors in tile colors of a desired image in r/place
 // @author       github.com/TotoShampoin
 // @match        https://garlic-bread.reddit.com/embed*
