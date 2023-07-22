c18b1a4b7ee2ad69127c5aca89206f389f67ae9f
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-07-22 10:39:21 +0200

    updated the reloc

--- relocator.user.js
+++ relocator.user.js
@@ -13,8 +13,8 @@ if (window.top !== window.self) {
   window.addEventListener('load', () => {
     const overlay = (function () {
         const img = document.createElement("img");
-        img.src = "https://cdn.discordapp.com/attachments/669939027537035297/1131644724529344542/hooky.png";
-        img.style = "position: absolute;left:500px;top:500px; width: 39px; height: 48px; image-rendering: pixelated; opacity: 0";
+        img.src = "https://cdn.discordapp.com/attachments/1131271629377392772/1132076481661653053/hookypolicepixel_punched.png";
+        img.style = "position: absolute;left:500px;top:500px; width: 16px; height: 22px; image-rendering: pixelated; opacity: 0";
         console.log(img);
         return img;
     })();
