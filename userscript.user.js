3b25d35ea6ca903d4adeb23663032c248f049545
TotoShampoin <thomasgarnier2010@hotmail.fr>
2023-07-23 13:02:57 +0200

    version

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/Place Overlay
 // @namespace    https://github.com/TotoShampoin/place-overlay-hypnohookup
-// @version      2.1.0
+// @version      2.2.0
 // @description  A visual overlay to show errors in tile colors of a desired image in r/place
 // @author       github.com/TotoShampoin
 // @match        https://garlic-bread.reddit.com/embed*
